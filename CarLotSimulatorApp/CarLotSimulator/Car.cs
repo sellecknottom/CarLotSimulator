d11f3028cb05311ecdb02b9e10e33308596426c1
Selleck Elliott <selleck.elliott@gmail.com>
2024-06-11 23:52:44 -0600

    car stories

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public Car() 
+        { 
+        }
+        //Custom Constructor
+        public Car(string make, string model, string year, string engineNoise, string honkNoise, string isDriveable)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+            IsDriveable = isDriveable;
+        }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public string IsDriveable { get; set; }
+
+        //methods
+        public void MakeEngineNoise()
+        {
+            Console.WriteLine("Vroom Vroom!");
+        }
+
+        public void MakeHonkNoise()
+        {
+            Console.WriteLine("Beep Beep!");
+        }
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CarLotSimulator
 {
@@ -17,6 +18,49 @@ namespace CarLotSimulator
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+
+            Car car1 = new Car();
+            //Dot Notation
+            car1.Make = "Buick";
+            car1.Model = "Enclave";
+            car1.Year = "2024";
+            car1.EngineNoise = "quiet";
+            car1.HonkNoise = "bright";
+            car1.IsDriveable = "driveable";
+
+            Console.WriteLine($"I need to buy a new car. I want a {car1.Year} {car1.Make} {car1.Model}.");
+            Console.WriteLine($"Since it is a new car, it will be {car1.EngineNoise}. The horn will be {car1.HonkNoise}" +
+                $"\nand it will be {car1.IsDriveable}");
+            car1.MakeEngineNoise();
+            car1.MakeHonkNoise();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Custom Constructor
+            Car car2 = new Car("Jeep", "Grand Cherokee", "2015", "needed work", "softer", "is currently driveable");
+            Console.WriteLine($"In march I bought a {car2.Year} {car2.Make} {car2.Model}. The engine {car2.EngineNoise}" +
+                $"\nand the horn is {car2.HonkNoise}, but it {car2.IsDriveable}");
+            car2.MakeHonkNoise();
+            car2.MakeEngineNoise();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //object initializer syntax
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            Car car3 = new Car()
+            {
+                Make = "Toyota",
+                Model = "Corolla",
+                Year = "2001",
+                EngineNoise = "loud",
+                HonkNoise = "nasally",
+                IsDriveable = "barely driveable"
+            };
+            Console.WriteLine($"My first car was a {car3.Year} {car3.Make} {car3.Model}. It was {car3.EngineNoise},\n" +
+                $"the horn sounded {car3.HonkNoise} and it was {car3.IsDriveable}.");
+            Console.WriteLine($"Annoying {car3.MakeHonkNoise}");
+            Console.WriteLine($"Not much {car3.MakeEngineNoise}");
+
 
             //*************BONUS*************//
 
